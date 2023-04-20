0f809e730658492fee9d702d8f501af8431ceb4e
Antonio Neto <112784569+ap-neto@users.noreply.github.com>
2023-04-20 18:07:49 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\woere\\Desktop\\Pogramacao\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\woere\\Desktop\\Pogramacao\\PDM\\projeto_pdm" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\woere\\Desktop\\Pogramacao\\flutter"
+  "PROJECT_DIR=C:\\Users\\woere\\Desktop\\Pogramacao\\PDM\\projeto_pdm"
+  "FLUTTER_ROOT=C:\\Users\\woere\\Desktop\\Pogramacao\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\woere\\Desktop\\Pogramacao\\PDM\\projeto_pdm\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\woere\\Desktop\\Pogramacao\\PDM\\projeto_pdm"
+  "FLUTTER_TARGET=C:\\Users\\woere\\Desktop\\Pogramacao\\PDM\\projeto_pdm\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\woere\\Desktop\\Pogramacao\\PDM\\projeto_pdm\\.dart_tool\\package_config.json"
+)
